f4354493a1f8edee92315d4e964f4fde6a46f489
Daniel Malone <danieljmalone@gmail.com>
2017-04-09 20:22:25 -0500

    initial commit

---
+++ app/src/main/java/com/finepointmobile/constraintrecycler/MainActivity.java
@@ -0,0 +1,34 @@
+package com.finepointmobile.constraintrecycler;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.Adapter mAdapter;
+    private ArrayList<String> mDataset;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDataset = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            mDataset.add("Welcome to # " + i);
+        }
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new MainAdapter(mDataset);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+}
